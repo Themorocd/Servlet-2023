53d1eb2eec8ffd6106d49c5147a00156baa2435c
Themorocd <73909321+Themorocd@users.noreply.github.com>
2023-09-18 09:35:01 +0200

    Update Actividad1.java

--- Actividad1/src/actividad1/Actividad1.java
+++ Actividad1/src/actividad1/Actividad1.java
@@ -18,15 +18,15 @@ public class Actividad1 {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
-    int num1,num2;
+    int num1,num2,total;
     
-    System.out.println("Introduce 2 numeros para sumar");
+    System.out.println("Introduce 2 numeros para sumar");//Aqui le pido que introduzca los 2 numeros que quiero sumar
     System.out.println("Introduce el numero 1");
-    num1 = sc.nextInt();
+    num1 = sc.nextInt();//introduzco el primer numero
     System.out.println("Introduce el numero 2");
-    num2 = sc.nextInt();
-    int total= num1+num2;
-        System.out.println("La suma de los numeros: "+num1+" y "+num2+" es: "+total);
+    num2 = sc.nextInt();//ahora el segundo 
+    total= num1+num2;//sumo ambos numeros
+        System.out.println("La suma de los numeros: "+num1+" y "+num2+" es: "+total);//los muestro por consola
     
     }
     

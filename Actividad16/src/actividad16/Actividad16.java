0b8f96a6b583791d42929d1bd0e88fefa6e18dc9
Themorocd <73909321+Themorocd@users.noreply.github.com>
2023-10-09 14:31:12 +0200

    .

--- Actividad16/src/actividad16/Actividad16.java
+++ Actividad16/src/actividad16/Actividad16.java
@@ -22,7 +22,46 @@ public class Actividad16 {
        Alimento alimento = new Alimento();
        
       
-       
+        System.out.println("Ingrese los datos del alimento:");
+        while (alimento.nombre == null || alimento.nombre.isEmpty()) {
+            System.out.print("Nombre del alimento: ");
+            alimento.nombre = sc.nextLine();
+        }
+
+        while (true) {
+            try {
+                System.out.print("Contenido en lípidos (%): ");
+                alimento.Lipidos = Double.parseDouble(sc.nextLine());
+
+                System.out.print("Contenido en hidratos de carbono (%): ");
+                alimento.Hidratos = Double.parseDouble(sc.nextLine());
+
+                System.out.print("Contenido en proteínas (%): ");
+                alimento.Proteinas = Double.parseDouble(sc.nextLine());
+
+                break; // Salir del bucle si se ingresan valores válidos
+            } catch (NumberFormatException e) {
+                System.out.println("Por favor, ingrese valores numéricos válidos para lípidos, hidratos y proteínas.");
+            }
+        }
+
+        System.out.print("¿Es de origen animal? (Sí/No): ");
+        String origenAnimalInput = sc.nextLine().trim().toLowerCase();
+        alimento.Animal = origenAnimalInput.equals("sí") || origenAnimalInput.equals("si");
+
+        while (true) {
+            System.out.print("Contenido de minerales y vitaminas (A/M/B): ");
+            String contenidoMineralesVitaminasInput = sc.nextLine().trim().toUpperCase();
+            if (contenidoMineralesVitaminasInput.equals("A") || contenidoMineralesVitaminasInput.equals("M") || contenidoMineralesVitaminasInput.equals("B")) {
+                alimento.MineralesVitaminas = contenidoMineralesVitaminasInput.charAt(0);
+                break; // Salir del bucle si se ingresa un valor válido
+            } else {
+                System.out.println("Por favor, ingrese un valor válido para minerales y vitaminas (A/M/B).");
+            }
+        }
+
+        // Mostrar información del alimento
+        alimento.mostrarAlimento();
         
     }
     
--- Actividad16/src/actividad16/Alimento.java
+++ Actividad16/src/actividad16/Alimento.java
@@ -9,17 +9,17 @@ package actividad16;
  * @author Andres
  */
 class Alimento {
-    private String nombre;
-    private double Lipidos;
-    private double Hidratos;
-    private double Proteinas;
-    private boolean Animal;
-    private String MineralesVitaminas;
+    String nombre;
+    double Lipidos;
+    double Hidratos;
+    double Proteinas;
+    boolean Animal;
+    char MineralesVitaminas;
 
     public Alimento() {
     }
 
-    public Alimento(String nombre, double Lipidos, double Hidratos, double Proteinas, boolean Animal, String MineralesVitaminas) {
+    public Alimento(String nombre, double Lipidos, double Hidratos, double Proteinas, boolean Animal, char MineralesVitaminas) {
         this.nombre = nombre;
         this.Lipidos = Lipidos;
         this.Hidratos = Hidratos;
@@ -27,5 +27,32 @@ class Alimento {
         this.Animal = Animal;
         this.MineralesVitaminas = MineralesVitaminas;                                                                                                                                                                                                                                                                                                   
     }
-    
+     public boolean esDietetico() {
+        return (Lipidos < 20) && (MineralesVitaminas != 'B' && MineralesVitaminas != 'b');
+    }
+
+    public boolean recomendableParaDeportistas() {
+        return (Proteinas >= 10 && Proteinas <= 15)
+                && (Lipidos >= 30 && Lipidos <= 35)
+                && (Hidratos >= 55 && Hidratos <= 65);
+    }
+
+    public double contenidoEnergetico() {
+        double caloriasLipidos = Lipidos * 9.4;
+        double caloriasProteinas = Proteinas * 5.3;
+        double caloriasHidratos = Hidratos * 4.1;
+        return caloriasLipidos + caloriasProteinas + caloriasHidratos;
+    }
+
+    public void mostrarAlimento() {
+        System.out.println("Nombre del alimento: " + nombre);
+        System.out.println("Contenido en lípidos (%): " + Lipidos);
+        System.out.println("Contenido en hidratos de carbono (%): " + Hidratos);
+        System.out.println("Contenido en proteínas (%): " + Proteinas);
+        System.out.println("Es de origen animal: " + (Animal ? "Sí" : "No"));
+        System.out.println("Contenido de minerales y vitaminas: " + MineralesVitaminas);
+        System.out.println("Es dietético: " + (esDietetico() ? "Sí" : "No"));
+        System.out.println("Recomendable para deportistas: " + (recomendableParaDeportistas() ? "Sí" : "No"));
+        System.out.println("Contenido energético (kcal/g): " + contenidoEnergetico());
+    }
 }

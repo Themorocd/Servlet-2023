fc40845814434e75e5cdb9b088593cd885d81a3b
Themorocd <73909321+Themorocd@users.noreply.github.com>
2023-10-16 10:32:10 +0200

    .

---
+++ Actividad18/src/actividad18/Actividad18.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package actividad18;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Andres
+ */
+public class Actividad18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        Libro libro = new Libro();
+        
+        System.out.println("Ingrese los datos del libro");
+        while (libro.nombre == null || libro.nombre.isEmpty()) {
+            
+            System.out.println("Nombre del libro");
+            
+            libro.nombre = sc.nextLine();
+            
+        }
+        
+        while (true) {            
+            try {
+                System.out.println("Numero de paginas: ");
+                libro.paginas = sc.nextInt();
+                
+                System.out.println("Numero de subtitulos: ");
+                libro.subtitulos = sc.nextInt();
+                
+                System.out.println("Numero de imagenes: ");
+                libro.img = sc.nextInt();
+                
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Ingrese numeros enteros");
+            }
+        }
+        
+        while (true) {            
+             System.out.println("Nacionalidad del autor (N o E)");
+             String tiponacionalidad = sc.nextLine().trim().toUpperCase();
+             if((tiponacionalidad.equals("N") || (tiponacionalidad.equals("E") ))){
+            libro.nacionalidad = tiponacionalidad.charAt(0);
+            break;
+        }else{
+                 System.out.println("Por favor, ingrese un tipo de nacionalidad valido");
+             }
+        }
+        
+        
+        
+        
+        while (true) {            
+             System.out.println("Tipo de tapa: (D/d o M/m o B/b)");
+             String tipotapa = sc.nextLine().trim().toUpperCase();
+             if((tipotapa.equals("D") || (tipotapa.equals("M") || tipotapa.equals("B")))){
+            libro.tapa = tipotapa.charAt(0);
+            break;
+        }else{
+                 System.out.println("Por favor, ingrese un tipo de tapa valido");
+             }
+        }
+       
+        
+         while (true) {            
+             System.out.println("Tipo de formato: (I/i o Ma/ma o Me/me");
+             String tipoformato = sc.nextLine().trim().toUpperCase();
+             if((tipoformato.equals("I") || (tipoformato.equals("Ma") || tipoformato.equals("Me")))){
+             libro.formato = tipoformato;
+             break;
+        }else{
+                 System.out.println("Por favor, ingrese un tipo de formato valido");
+             }
+        }
+        
+        
+        
+        
+    }
+    
+}
---
+++ Actividad18/src/actividad18/Libro.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividad18;
+
+/**
+ *
+ * @author Andres
+ */
+class Libro {
+    
+    String nombre;
+    int paginas,subtitulos,img;
+    char nacionalidad,tapa;
+    String formato;
+    double tinta;
+    public Libro() {
+    }
+
+    public Libro(String nombre, int paginas, int subtitulos, int img, char nacionalidad, char tapa, String formato, double tinta) {
+        this.nombre = nombre;
+        this.paginas = paginas;
+        this.subtitulos = subtitulos;
+        this.img = img;
+        this.nacionalidad = nacionalidad;
+        this.tapa = tapa;
+        this.formato = formato;
+        this.tinta = tinta;
+    }
+
+  
+    
+    
+    public void monstrarLibro(){
+        System.out.println("Nombre del libro: "+nombre);
+        System.out.println("Analisis del libro");
+        System.out.println("-Paginas: "+paginas);
+        System.out.println("-Subtitulos: "+subtitulos);
+        System.out.println("-Imagenes: "+img);
+        System.out.println("Tipo de tapa: "+tapa);
+        System.out.println("Tipo de formato: "+formato);
+        System.out.println(librorecomendable());
+        System.out.println(librovistoso());
+        System.out.println("Nacionalidad del autor: ");
+        System.out.println("Precio en tinta es de "+preciotinta());
+    }
+
+    public String librovistoso() {
+         if ((paginas > 120) && (formato == "me" && formato == "ME")){
+             return "No es un libro vistoso";
+         }else{
+             return "Es un libro vistoso";
+         }
+        
+    }
+
+    public String librorecomendable() {
+        
+        if((paginas >=160 && paginas <=430) && (subtitulos >=6 && subtitulos <= 15) && (img >= 15 && img <=24)){
+            
+            return "El libro es recomendable";
+            
+        } else{
+            return "El libro no es recomendable";
+        }
+        
+        
+    }
+
+    public double preciotinta() {
+        
+       double preciototal = (paginas*0.05) + (subtitulos*0.01) + (img*0.07);
+        
+        return preciototal;
+    }
+    
+}

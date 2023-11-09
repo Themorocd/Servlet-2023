952ad1eb118588668fd777f70b9e90a57d8e3b41
Themorocd <73909321+Themorocd@users.noreply.github.com>
2023-11-09 08:38:19 +0100

    Correciones
    Correcion de errores no vistos con anterioridad

--- Tema2.Ejercicio4/src/java/Servlets/servletformu.java
+++ Tema2.Ejercicio4/src/java/Servlets/servletformu.java
@@ -42,7 +42,7 @@ public class servletformu extends HttpServlet {
         String texto = request.getParameter("texto");
         
         if(actividad == null){
-            rd = request.getRequestDispatcher("/Formulario.jsp");
+            rd = request.getRequestDispatcher("/ErrorFormu.html");
             rd.forward(request, response);
         }else{
         Actividades actividades = new Actividades(name,lugar,fecha,hora,destino,curso,actividad,profe,texto,usuario);

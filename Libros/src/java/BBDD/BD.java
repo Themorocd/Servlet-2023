a0ab449edd85098c7eba033e5796fe2c5ea08581
Andres Jose Marin Pascualvaca <73909321+Themorocd@users.noreply.github.com>
2024-01-09 17:49:23 +0100

    .

---
+++ Libros/src/java/BBDD/BD.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BBDD;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author moro-
+ */
+public class BD {
+    public static String usuario = "root";
+    public static String password = "";
+    public static String servidor = "localhost:3306";
+    public static String basededatos = "examen";
+    
+     public static Connection CrearConexion() {
+
+        Connection cnn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String url = "jdbc:mysql://" + servidor + "/" + basededatos;
+            cnn = DriverManager.getConnection(url, usuario, password);
+        } catch (ClassNotFoundException c) {
+            System.out.println("Controlador JBDC no encontrado" + c.toString());
+        } catch (Exception c) {
+            System.out.println("Otra excepcion" + c.toString());
+        }
+        return cnn;
+    }
+
+    public static boolean compruebouser(String user, String pass) {
+        
+        Connection cnn = null;
+        
+        try{
+        cnn = CrearConexion();
+        String sql = "SELECT * FROM `usuario` WHERE `usuario`='" + user + "' AND `password`='" + pass + "'";
+        
+        PreparedStatement smt = cnn.prepareStatement(sql);
+        ResultSet rs = smt.executeQuery();
+        if(rs.next()){
+            return true;
+        }
+        
+        }catch (SQLException ex) {
+            Logger.getLogger(BD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+        
+    }
+}
---
+++ Libros/web/Gestion.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : Gestion
+    Created on : 9 ene 2024, 17:20:13
+    Author     : moro-
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
